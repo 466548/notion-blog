681573b143310ddc1a410d8d5151cb193a7f54d2
466548 <tsuka.as@gmail.com>
2021-08-16 23:21:45 +0900

    add

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -13,22 +13,12 @@ const contacts = [
   {
     Comp: Twitter,
     alt: 'twitter icon',
-    link: 'https://twitter.com/_ijjk',
+    link: 'https://twitter.com/466548',
   },
   {
     Comp: GitHub,
     alt: 'github icon',
-    link: 'https://github.com/ijjk',
-  },
-  {
-    Comp: LinkedIn,
-    alt: 'linkedin icon',
-    link: 'https://www.linkedin.com/in/jj-kasper-0b5392166/',
-  },
-  {
-    Comp: Envelope,
-    alt: 'envelope icon',
-    link: 'mailto:jj@jjsweb.site?subject=Notion Blog',
+    link: 'https://github.com/466548',
   },
 ]
 
